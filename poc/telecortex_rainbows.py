4d29b15d2062a9db659f187b8e6ee2a5b9541fee
derwentx <derwentx@gmail.com>
2018-03-13 18:46:52 +0800

    utils in own file

--- poc/telecortex_rainbows.py
+++ poc/telecortex_rainbows.py
@@ -1,22 +1,20 @@
+"""
+Rainbow test script
+"""
+
 from __future__ import unicode_literals
 
-import base64
 import itertools
 import logging
 import os
-import re
-import time
-from collections import OrderedDict
 from datetime import datetime
-from pprint import pformat, pprint
 
 import serial
 from serial.tools import list_ports
 
 import coloredlogs
-import six
-from kitchen.text import converters
 from telecortex_session import TelecortexSession
+from telecortex_utils import pix_array2text
 
 STREAM_LOG_LEVEL = logging.INFO
 # STREAM_LOG_LEVEL = logging.WARN
@@ -58,27 +56,6 @@ PANEL_LENGTHS = [
 
 DO_SINGLE = False
 
-def pix_array2text(*pixels):
-    """Convert an array of pixels to a base64 encoded unicode string."""
-    # logging.debug("pixels: %s" % repr(["%02x" % pixel for pixel in pixels]))
-    # logging.debug(
-    #     "bytes: %s" % repr([six.int2byte(pixel) for pixel in pixels])
-    # )
-    pix_bytestring = b''.join([
-        six.int2byte(pixel % 256)
-        for pixel in pixels
-    ])
-    # logging.debug("bytestring: %s" % repr(pix_bytestring))
-
-    response = base64.b64encode(pix_bytestring)
-    response = six.text_type(response, 'ascii')
-    # response = ''.join(map(six.unichr, pixels))
-    # response = six.binary_type(base64.b64encode(
-    #     bytes(pixels)
-    # ))
-    # logging.debug("pix_text: %s" % repr(response))
-    return response
-
 
 def main():
     """
@@ -89,7 +66,6 @@ def main():
     Rend some HSV rainbowz
     Respond to microcontroller
     """
-
     logging.debug("\n\n\nnew session at %s" % datetime.now().isoformat())
 
     target_device = TELECORTEX_DEV
