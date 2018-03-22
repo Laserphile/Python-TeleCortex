9909221fcedb84b965b5d20e96dc47d8048d3c98
derwentx <derwentx@gmail.com>
2018-03-22 12:00:53 +0800

    Change panel data structure

--- poc/steamed_hams.py
+++ poc/steamed_hams.py
@@ -50,7 +50,7 @@ ANIM_SPEED = 10
 MAIN_WINDOW = 'image_window'
 INTERPOLATION_TYPE = 'bilinear'
 # INTERPOLATION_TYPE = 'nearest'
-DOT_RADIUS = 2
+DOT_RADIUS = 3
 
 def draw_map(image, pix_map_normlized, outline=None):
     """Given an image and a normalized pixel map, draw the map on the image."""
@@ -66,16 +66,27 @@ def draw_map(image, pix_map_normlized, outline=None):
 
 MON = {'top': 100, 'left': 0, 'width': 200, 'height': 200}
 
-PANELS = [
-    # (0, 0, 'big'),
-    (2, 1, 'smol'),
-    (2, 2, 'smol'),
-    # (2, 3, 'smol'),
-    (3, 1, 'smol'),
-    (3, 2, 'smol'),
-    (3, 3, 'smol')
-]
+PANELS = OrderedDict([
+    (2, [
+        (1, 'smol'),
+        (2, 'smol')
+    ]),
+    (3, [
+        (1, 'smol'),
+        (2, 'smol'),
+        (3, 'smol')
+    ])
+])
 
+# PANELS = [
+#     # (0, 0, 'big'),
+#     (2, 1, 'smol'),
+#     (2, 2, 'smol'),
+#     # (2, 3, 'smol'),
+#     (3, 1, 'smol'),
+#     (3, 2, 'smol'),
+#     (3, 3, 'smol')
+# ]
 
 def main():
     """
@@ -126,26 +137,27 @@ def main():
         )
         pixel_str_smol = pix_array2text(*pixel_list_smol)
         pixel_str_big = pix_array2text(*pixel_list_big)
-        for server_id, panel_number, size in PANELS:
-            if size == 'big':
-                pixel_str = pixel_str_big
-            elif size == 'smol':
-                pixel_str = pixel_str_smol
+        for server_id, server_panel_info in PANELS.items():
+            for panel_number, size in server_panel_info:
+                if size == 'big':
+                    pixel_str = pixel_str_big
+                elif size == 'smol':
+                    pixel_str = pixel_str_smol
 
-            manager.sessions[server_id].chunk_payload(
-                "M2600", "Q%d" % panel_number, pixel_str
-            )
+                manager.sessions[server_id].chunk_payload(
+                    "M2600", "Q%d" % panel_number, pixel_str
+                )
             manager.sessions[server_id].send_cmd_sync('M2610')
 
-            if ENABLE_PREVIEW:
-                draw_map(img, pix_map_normlized_smol)
-                draw_map(img, pix_map_normlized_big, outline=(255, 255, 255))
-                cv2.imshow(MAIN_WINDOW, img)
-            if int(time_now() * TARGET_FRAMERATE / 2) % 2 == 0:
-                key = cv2.waitKey(2) & 0xFF
-                if key == 27:
-                    cv2.destroyAllWindows()
-                    break
+        if ENABLE_PREVIEW:
+            draw_map(img, pix_map_normlized_smol)
+            draw_map(img, pix_map_normlized_big, outline=(255, 255, 255))
+            cv2.imshow(MAIN_WINDOW, img)
+        if int(time_now() * TARGET_FRAMERATE / 2) % 2 == 0:
+            key = cv2.waitKey(2) & 0xFF
+            if key == 27:
+                cv2.destroyAllWindows()
+                break
 
 
 
