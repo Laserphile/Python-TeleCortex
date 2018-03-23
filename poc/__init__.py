819af3fb663b1fa2c5d4ecdfede1a0b84adc95cb
derwentx <derwentx@gmail.com>
2018-03-23 08:20:26 +0800

    update poc doc

--- poc/__init__.py
+++ poc/__init__.py
@@ -1,1 +1,10 @@
-"""Proof of concept scripts."""
+"""
+
+Proof of concept scripts.
+
+rainbows.py : single-controller, draws the same rainbow on all panels
+session.py : Multiple controller, draws the same rainbow on all panels on all controllers
+linalg.py : Multiple controller, uses multiple mappings to draw different parts of the same rainbow on a panel on each controller
+steamed_hams.py : Multiple controller, uses multiple mappings to draw different parts of a screendump on a panel on each controller
+
+"""
