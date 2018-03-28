1db95ec9739cb16f2fb8d268c6e5d9d281555aaa
derwentx <derwentx@gmail.com>
2018-03-28 16:42:37 +0800

    directly write circles on to each panel (without OpenCV or Image)

--- poc/parallel.py
+++ poc/parallel.py
@@ -3,10 +3,11 @@ import colorsys
 import itertools
 import logging
 import multiprocessing as mp
+import math
 import os
 from collections import OrderedDict
 from time import time as time_now
-
+from pprint import pformat
 
 import serial
 
@@ -28,8 +29,7 @@ STREAM_LOG_LEVEL = logging.WARN
 # STREAM_LOG_LEVEL = logging.ERROR
 
 LOG_FILE = ".parallel.log"
-ENABLE_LOG_FILE = False
-ENABLE_PREVIEW = True
+ENABLE_LOG_FILE = True
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.DEBUG)
@@ -57,27 +57,27 @@ DOT_RADIUS = 0
 
 SERVERS = OrderedDict([
     (0, {
-        'file': '/dev/cu.usbmodem3176951',
-        'baud': 57600,
-        'timeout': 1
-    }),
-    (1, {
         'file': '/dev/cu.usbmodem4057531',
         'baud': 57600,
         'timeout': 1
     }),
+    (1, {
+        'file': '/dev/cu.usbmodem4058621',
+        'baud': 57600,
+        'timeout': 1
+    }),
     (2, {
-        'file': '/dev/cu.usbmodem4057541',
+        'file': '/dev/cu.usbmodem3176951',
         'baud': 57600,
         'timeout': 1
     }),
     (3, {
-        'file': '/dev/cu.usbmodem4058601',
+        'file': '/dev/cu.usbmodem4057541',
         'baud': 57600,
         'timeout': 1
     }),
     (4, {
-        'file': '/dev/cu.usbmodem4058621',
+        'file': '/dev/cu.usbmodem4058601',
         'baud': 57600,
         'timeout': 1
     }),
@@ -125,13 +125,30 @@ PANELS = OrderedDict([
     ])
 ])
 
-def fill_rainbows(image, angle=0.0):
+def fill_rainbows(image, angle=0.):
     for col in range(IMG_SIZE):
         hue = (col * MAX_HUE / IMG_SIZE + angle * MAX_HUE / MAX_ANGLE ) % MAX_HUE
-        rgb = tuple(c * 255 for c in colorsys.hls_to_rgb(hue, 0.5, 1))
+        rgb = tuple(int(c * 255) for c in colorsys.hls_to_rgb(hue, 0.5, 1))
         # logging.debug("rgb: %s" % (rgb,))
         cv2.line(image, (col, 0), (col, IMG_SIZE), color=rgb, thickness=1)
     return image
+
+def direct_rainbows(pix_map, angle=0.):
+    pixel_list = []
+    for coordinate in pix_map:
+        magnitude = math.sqrt(
+            (0.5 - coordinate[0]) ** 2 +
+            (0.5 - coordinate[1]) ** 2
+        )
+        hue = (magnitude * MAX_HUE + angle * MAX_HUE / MAX_ANGLE ) % MAX_HUE
+        rgb = tuple(int(c * 255) for c in colorsys.hls_to_rgb(hue, 0.5, 1))
+        logging.debug("rgb: %s" % (rgb,))
+        pixel_list.append(rgb)
+
+    logging.debug("pixel_list: %s" % pformat(pixel_list))
+    pixel_list = list(itertools.chain(*pixel_list))
+    logging.debug("pixel_list returned: %s ... " % (pixel_list[:10]))
+    return pixel_list
 
 def main():
 
@@ -140,29 +157,16 @@ def main():
     pix_map_normlized_smol = normalize_pix_map(PIXEL_MAP_SMOL)
     pix_map_normlized_big = normalize_pix_map(PIXEL_MAP_BIG)
 
-    img = np.ndarray(shape=(IMG_SIZE, IMG_SIZE, 3), dtype=np.uint8)
-
-    if ENABLE_PREVIEW:
-        window_flags = 0
-        window_flags |= cv2.WINDOW_NORMAL
-        # window_flags |= cv2.WINDOW_AUTOSIZE
-        # window_flags |= cv2.WINDOW_FREERATIO
-        window_flags |= cv2.WINDOW_KEEPRATIO
-
-        cv2.namedWindow(MAIN_WINDOW, flags=window_flags)
-        cv2.imshow(MAIN_WINDOW, img)
-
     start_time = time_now()
 
     while manager:
         frameno = ((time_now() - start_time) * TARGET_FRAMERATE * ANIM_SPEED) % MAX_ANGLE
-        fill_rainbows(img, frameno)
 
-        pixel_list_smol = interpolate_pixel_map(
-            img, pix_map_normlized_smol, INTERPOLATION_TYPE
+        pixel_list_smol = direct_rainbows(
+            pix_map_normlized_smol, frameno
         )
-        pixel_list_big = interpolate_pixel_map(
-            img, pix_map_normlized_big, INTERPOLATION_TYPE
+        pixel_list_big = direct_rainbows(
+            pix_map_normlized_big, frameno
         )
         pixel_str_smol = pix_array2text(*pixel_list_smol)
         pixel_str_big = pix_array2text(*pixel_list_big)
