c746520c7eef11c64d2273f27dc2d09bcec76524
Jonathan Van Buren <jvburen@thoughtworks.com>
2018-03-28 17:36:58 +0800

    did a refactor to put panel 'driver' code in its own class

---
+++ poc/parallel_jvb.py
@@ -0,0 +1,163 @@
+import colorsys
+import itertools
+import logging
+import multiprocessing as mp
+import math
+import os
+from collections import OrderedDict
+from time import time as time_now
+from pprint import pformat
+
+import serial
+
+import coloredlogs
+import cv2
+import numpy as np
+from rainbow_driver import PanelDriver
+from context import telecortex
+from telecortex.session import (DEFAULT_BAUDRATE, DEFAULT_TIMEOUT,
+                                PANEL_LENGTHS, TelecortexSession, TelecortexThreadManager, SERVERS)
+from telecortex.interpolation import interpolate_pixel_map
+from telecortex.mapping import (PIXEL_MAP_BIG, PIXEL_MAP_SMOL, PANELS,
+                                normalize_pix_map, rotate_mapping, scale_mapping, rotate_vector,
+                                transpose_mapping, draw_map)
+from telecortex.util import pix_array2text
+
+# STREAM_LOG_LEVEL = logging.DEBUG
+# STREAM_LOG_LEVEL = logging.INFO
+STREAM_LOG_LEVEL = logging.WARN
+# STREAM_LOG_LEVEL = logging.ERROR
+
+LOG_FILE = ".parallel.log"
+ENABLE_LOG_FILE = True
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.DEBUG)
+FILE_HANDLER = logging.FileHandler(LOG_FILE)
+FILE_HANDLER.setLevel(logging.DEBUG)
+STREAM_HANDLER = logging.StreamHandler()
+STREAM_HANDLER.setLevel(STREAM_LOG_LEVEL)
+if os.name != 'nt':
+    STREAM_HANDLER.setFormatter(coloredlogs.ColoredFormatter())
+STREAM_HANDLER.addFilter(coloredlogs.HostNameFilter())
+STREAM_HANDLER.addFilter(coloredlogs.ProgramNameFilter())
+if ENABLE_LOG_FILE:
+    LOGGER.addHandler(FILE_HANDLER)
+LOGGER.addHandler(STREAM_HANDLER)
+
+IMG_SIZE = 256
+MAX_HUE = 1.0
+MAX_ANGLE = 360
+TARGET_FRAMERATE = 20
+ANIM_SPEED = 5
+MAIN_WINDOW = 'image_window'
+# INTERPOLATION_TYPE = 'bilinear'
+INTERPOLATION_TYPE = 'nearest'
+DOT_RADIUS = 0
+
+SERVERS = OrderedDict([
+    (0, {
+        'file': '/dev/cu.usbmodem4057531',
+        'baud': 57600,
+        'timeout': 1
+    }),
+    (1, {
+        'file': '/dev/cu.usbmodem4058621',
+        'baud': 57600,
+        'timeout': 1
+    }),
+    (2, {
+        'file': '/dev/cu.usbmodem3176951',
+        'baud': 57600,
+        'timeout': 1
+    }),
+    (3, {
+        'file': '/dev/cu.usbmodem4057541',
+        'baud': 57600,
+        'timeout': 1
+    }),
+    (4, {
+        'file': '/dev/cu.usbmodem4058601',
+        'baud': 57600,
+        'timeout': 1
+    }),
+])
+
+# Uncomment for Derwent config
+# SERVERS = OrderedDict([
+#     (1, {
+#         'file': '/dev/cu.usbmodem144101',
+#         'baud': 57600,
+#         'timeout': 1
+#     }),
+# ])
+
+PANELS = OrderedDict([
+    (0, [
+        (0, 'big'),
+        (1, 'smol'),
+        (2, 'smol'),
+        (3, 'smol')
+    ]),
+    (1, [
+        (0, 'big'),
+        (1, 'smol'),
+        (2, 'smol'),
+        (3, 'smol')
+    ]),
+    (2, [
+        (0, 'big'),
+        (1, 'smol'),
+        (2, 'smol'),
+        (3, 'smol')
+    ]),
+    (3, [
+        (0, 'big'),
+        (1, 'smol'),
+        (2, 'smol'),
+        (3, 'smol')
+    ]),
+    (4, [
+        (0, 'big'),
+        (1, 'smol'),
+        (2, 'smol'),
+        (3, 'smol')
+    ])
+])
+
+def main():
+    manager = TelecortexThreadManager(SERVERS)
+
+    pix_map_normlized_smol = normalize_pix_map(PIXEL_MAP_SMOL)
+    pix_map_normlized_big = normalize_pix_map(PIXEL_MAP_BIG)
+
+    start_time = time_now()
+
+    while manager:
+        frameno = ((time_now() - start_time) * TARGET_FRAMERATE * ANIM_SPEED) % MAX_ANGLE
+
+        driver = PanelDriver(pix_map_normlized_smol, pix_map_normlized_big, IMG_SIZE, MAX_HUE, MAX_ANGLE)
+
+        pixel_list_smol, pixel_list_big = driver.direct_rainbows(frameno)
+
+        pixel_str_smol = pix_array2text(*pixel_list_smol)
+        pixel_str_big = pix_array2text(*pixel_list_big)
+        for server_id, server_panel_info in PANELS.items():
+            if not manager.threads.get(server_id):
+                continue
+            for panel_number, size in server_panel_info:
+                if size == 'big':
+                    pixel_str = pixel_str_big
+                elif size == 'smol':
+                    pixel_str = pixel_str_smol
+
+                manager.threads[server_id][0].send(("M2600", {"Q": panel_number}, pixel_str))
+
+        for server_id, (pipe, proc) in manager.threads.items():
+            pipe.send(("M2610", None, None))
+
+        frameno = (frameno + 1) % 255
+
+
+if __name__ == '__main__':
+    main()
---
+++ poc/rainbow_driver.py
@@ -0,0 +1,51 @@
+import cv2
+import colorsys
+import itertools
+import logging
+import math
+from pprint import pformat
+
+
+class PanelDriver(object):
+
+    def __init__(self, pix_map_normlized_smol, pix_map_normlized_big, img_size, max_hue, max_angle):
+        self.pix_map_normlized_smol = pix_map_normlized_smol
+        self.pix_map_normlized_big = pix_map_normlized_big
+        self.img_size = img_size
+        self.max_hue = max_hue
+        self.max_angle = max_angle
+
+    def direct_rainbows(self, angle=0.):
+
+        pixel_list_smol = []
+        pixel_list_big = []
+        for coordinate in self.pix_map_normlized_smol:
+            self.calc_direct_rainbows(angle, coordinate, pixel_list_smol)
+        for coordinate in self.pix_map_normlized_big:
+            self.calc_direct_rainbows(angle, coordinate, pixel_list_big)
+
+        logging.debug("pixel_list: %s" % pformat(pixel_list_smol))
+        logging.debug("pixel_list: %s" % pformat(pixel_list_big))
+        pixel_list_smol = list(itertools.chain(*pixel_list_smol))
+        pixel_list_big = list(itertools.chain(*pixel_list_big))
+        logging.debug("pixel_list returned: %s ... " % (pixel_list_smol[:10]))
+        logging.debug("pixel_list returned: %s ... " % (pixel_list_big[:10]))
+        return pixel_list_smol, pixel_list_big
+
+    def calc_direct_rainbows(self, angle, coordinate, pixel_list):
+        magnitude = math.sqrt(
+            (0.5 - coordinate[0]) ** 2 +
+            (0.5 - coordinate[1]) ** 2
+        )
+        hue = (magnitude * self.max_hue + angle * self.max_hue / self.max_angle) % self.max_hue
+        rgb = tuple(int(c * 255) for c in colorsys.hls_to_rgb(hue, 0.5, 1))
+        logging.debug("rgb: %s" % (rgb,))
+        pixel_list.append(rgb)
+
+    # def fill_rainbows(self, image, angle=0.):
+    #     for col in range(self.img_size):
+    #         hue = (col * self.max_hue / self.img_size + angle * self.max_hue / self.max_angle) % self.max_hue
+    #         rgb = tuple(int(c * 255) for c in colorsys.hls_to_rgb(hue, 0.5, 1))
+    #         # logging.debug("rgb: %s" % (rgb,))
+    #         cv2.line(image, (col, 0), (col, self.img_size), color=rgb, thickness=1)
+    #     return image
