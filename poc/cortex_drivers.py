72086032bcc2d257c2692d5fef6cb497008897a2
Jonathan Van Buren <jvburen@thoughtworks.com>
2018-04-01 17:55:27 +0800

    trying something cray

--- poc/cortex_drivers.py
+++ poc/cortex_drivers.py
@@ -43,6 +43,34 @@ class PanelDriver(object):
         # log_if_correct_level(logging.DEBUG, "pixel_list: %s" % pformat(pixel_list))
         return list(itertools.chain(*pixel_list))
 
+    def wtf_jvb_rainbows(self, angle=0., seed=random.random()):
+        pixel_list_smol = self.calc_wtf_jvb_rainbows(angle, self.pix_map_normlized_smol, seed)
+        pixel_list_big = self.calc_wtf_jvb_rainbows(angle, self.pix_map_normlized_big, seed)
+        return pixel_list_smol, pixel_list_big
+
+    def calc_wtf_jvb_rainbows(self, angle, pix_map_normlized, seed):
+        pixel_list = []
+        speed_factor = 500
+        brightness_factor = 0.3
+        saturation = 1
+        hue = 1
+        value = 1
+
+        for coordinate in pix_map_normlized:
+            center = (0.5, 0.5)
+            magnitude = (
+                                (center[0] - coordinate[0]) ** 2 +
+                                (center[1] - coordinate[1]) ** 2
+                        )**(1/2)
+
+            sin_baby = angle/speed_factor
+            hue = math.sin((magnitude**2 + hue**2)/2*(magnitude + sin_baby/hue) + sin_baby)
+            # value = math.sin(-(magnitude**2 + value**2)/2*(sin_baby + value) + magnitude/2) * brightness_factor
+            value = 1
+            rgb = tuple(int(c * 255) for c in colorsys.hsv_to_rgb(hue, saturation, value))
+            pixel_list.append(rgb)
+        return list(itertools.chain(*pixel_list))
+
     def crazy_rainbows(self, angle=0., seed=random.random()):
         pixel_list_smol = self.calc_crazy_rainbows(angle, self.pix_map_normlized_smol, seed)
         pixel_list_big = self.calc_crazy_rainbows(angle, self.pix_map_normlized_big, seed)
