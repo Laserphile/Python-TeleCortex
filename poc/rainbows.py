762e4fc987a9af7bf6d212c53c086e678e039860
derwentx <derwentx@gmail.com>
2018-12-14 18:35:09 +1100

    placate linters

--- poc/rainbows.py
+++ poc/rainbows.py
@@ -1,23 +1,19 @@
-"""Simple Rainbow test script with no mapping required. """
+"""Simple Rainbow test script with no mapping required."""
 
 from __future__ import unicode_literals
 
 import itertools
 import logging
-import os
 from datetime import datetime
-import time
-
-import serial
-from serial.tools import list_ports
 from pprint import pformat
 
-import coloredlogs
+import serial
+
 from context import telecortex
-from telecortex.session import (PANEL_LENGTHS, DEFAULT_BAUD,
-                                TEENSY_VID, find_serial_dev, TelecortexSession)
-from telecortex.util import pix_array2text
 from telecortex.config import TeleCortexSessionConfig
+from telecortex.session import DEFAULT_BAUD, TEENSY_VID, find_serial_dev
+from telecortex.util import pix_array2text
+
 
 def main():
     """
@@ -28,15 +24,20 @@ def main():
     Rend some HSV rainbowz
     Respond to microcontroller
     """
-
     conf = TeleCortexSessionConfig(
         name="rainbowz",
-        description="send rainbows to a single telecortex controller as fast as possible",
+        description=(
+            "send rainbows to a single telecortex "
+            "controller as fast as possible"
+        ),
         default_config='single'
     )
     conf.parser.add_argument(
         '--do-single', default=False, action='store_true',
-        help="if true, send a single colour to the board, otherwise send a rainbow string"
+        help=(
+            "if true, send a single colour to the board, "
+            "otherwise send a rainbow string"
+        )
     )
     # conv.parser.add_argument(
     #     '--virtual', default=False, action='store_true',
@@ -73,7 +74,8 @@ def main():
                     pixel_str = pix_array2text(
                         frameno, 255, 127
                     )
-                    sesh.send_cmd_with_linenum("M2603", {"Q":panel, "V":pixel_str})
+                    sesh.send_cmd_with_linenum(
+                        "M2603", {"Q": panel, "V": pixel_str})
                 else:
                     panel_length = len(conf.maps[map_name])
                     logging.debug(
@@ -86,13 +88,13 @@ def main():
                     logging.debug("pixel_list: %s" % pformat(pixel_list))
                     pixel_list = list(itertools.chain(*pixel_list))
                     pixel_str = pix_array2text(*pixel_list)
-                    sesh.chunk_payload_with_linenum("M2601", {"Q":panel}, pixel_str)
+                    sesh.chunk_payload_with_linenum(
+                        "M2601", {"Q": panel}, pixel_str)
 
                 while not sesh.ready:
                     logging.debug("waiting on queue")
             sesh.send_cmd_with_linenum("M2610")
             frameno = (frameno + 1) % 255
-
 
 
 if __name__ == '__main__':
